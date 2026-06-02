20ad95f24effe641e6432dbe2cee849d5da15db4
Aniket Singh <singhaniket10791@gmail.com>
2026-06-02 18:14:04 +0530

    Add assignment 02 question 1 solution

---
+++ assingment_02_quest_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Print the first 10 natural numbers using a while loop
+
+    cout << "The natural numbers are: ";
+    int i = 1;
+    while (i <= 10)
+    {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
