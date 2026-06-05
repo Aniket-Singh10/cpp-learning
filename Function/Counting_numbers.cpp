b96f02a5531b538cfcc84e256957da7688800f1b
Aniket Singh <singhaniket10791@gmail.com>
2026-06-05 15:05:02 +0530

    counting numbers

---
+++ Function/Counting_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void counting(int a)
+{
+    for (int i = 1; i <= a; i++)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    counting(a);
+
+    return 0;
+}
